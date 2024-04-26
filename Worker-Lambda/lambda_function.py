abf2bcdb5af0a75d9bc88bfdd5b2dcb86818b1f6
gregorcaf <gregor.caf@gmail.com>
2024-04-27 00:19:55 +0200

    Set up Lambda layers and function logic to make gRPC calls to the API gateway provided by ngrok

--- Worker-Lambda/lambda_function.py
+++ Worker-Lambda/lambda_function.py
@@ -1,6 +1,9 @@
 import json
-import boto3
+# import boto3
 import math
+import grpc
+from Proto import master_pb2_grpc
+from Proto import master_pb2
 
 def lambda_handler(event, context):
     bucket_name = event["data_bucket_name"]
@@ -8,3 +11,19 @@ def lambda_handler(event, context):
 
     print("Bucket name: {}".format(bucket_name))
     print("Filenames {}".format(filenames))
+
+    ip_address = "7.tcp.eu.ngrok.io:19283"
+
+    # create a channel
+    channel = grpc.insecure_channel(ip_address)
+    
+    # create a stub
+    stub = master_pb2_grpc.masterStub(channel)
+
+    # create request object
+    request = master_pb2.PutDataRequest(key="city", value="Dubai")
+
+    # make gRPC call
+    response = stub.putData(request)
+
+    print(response)
--- test.py
+++ test.py
@@ -3,22 +3,21 @@ from Proto import master_pb2_grpc
 from Proto import master_pb2
 
 
-ip_address = "localhost"
-port = "9090"
+ip_address = "7.tcp.eu.ngrok.io:19283"
+# port = "9090"
 
 # create a channel
-channel = grpc.insecure_channel("{}:{}".format(ip_address, port))
+channel = grpc.insecure_channel(ip_address)
+# channel = grpc.insecure_channel("{}:{}".format(ip_address, port))
 
 # create a stub
 stub = master_pb2_grpc.masterStub(channel)
 
 # create request object
-request = master_pb2.PutDataRequest(key="City", value="Maribor")
+request = master_pb2.PutDataRequest(key="city", value="Dubai")
 
 # make gRPC call
 response = stub.putData(request)
 
 print(response)
-# print("Response code:", response.response_code)
-# print("Response message:", response.response_message)
 
